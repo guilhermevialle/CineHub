afd13b320eea36c7b0e2ecba28a77c58df9b0586
guilhermevialle <vialleguilherme@gmail.com>
2023-06-06 08:38:32 -0300

    fixing layout again

--- src/utils/get-genre.ts
+++ src/utils/get-genre.ts
@@ -1,8 +1,10 @@
 import { movieGenres } from '@/components/contexts/genres'
 
 export function getGenreId(name: string) {
-  const genre = movieGenres.find(
-    (genre) => genre.name.toLowerCase() === name.toLowerCase()
-  )
-  return genre ? genre.id : null
+  if (name) {
+    const genre = movieGenres.find(
+      (genre) => genre.name.toLowerCase() === name.toLowerCase()
+    )
+    return genre ? genre.id : null
+  }
 }
